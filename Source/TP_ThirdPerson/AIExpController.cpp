d722540efe9a92edd9c773ce732c0c9e0ddf0325
Pranav Paharia <pranavpaharia@hotmail.com>
2017-10-17 21:31:07 +0530

    Initial commit

---
+++ Source/TP_ThirdPerson/AIExpController.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TP_ThirdPerson.h"
+#include "AIExpController.h"
+
+
+
+
---
+++ Source/TP_ThirdPerson/AIExpController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AIController.h"
+#include "AIExpController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TP_THIRDPERSON_API AAIExpController : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
